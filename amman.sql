755dabe22a87e81fe9f781eaf6f6f8c18b18737d
Suthishan <suthishan@server2008.com>
2020-10-21 18:38:26 +0530

    Init

---
+++ amman.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2020 at 03:07 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amman`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment_list`
+--
+
+CREATE TABLE `payment_list` (
+  `id` int(100) NOT NULL,
+  `name` varchar(1000) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `mobile` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `message` varchar(1000) NOT NULL,
+  `payment_id` varchar(100) NOT NULL,
+  `payment_date` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `payment_list`
+--
+ALTER TABLE `payment_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `payment_list`
+--
+ALTER TABLE `payment_list`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
